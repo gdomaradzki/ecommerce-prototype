c1662d0d85f67a1b787b4a68f9e9654612ce71fa
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2017-04-13 21:43:34 -0300

    Added Wish List button on Product

--- src/components/modules/Product.vue
+++ src/components/modules/Product.vue
@@ -1,6 +1,11 @@
 <template>
   <article class="l-product">
     <img class="md-product__photo" src="./../../assets/img/men/1.jpg" alt="asad">
+    <button class="md-product__wish-list-button">
+      <svg class="md-wish-list__icon" viewBox="0 6 20 20">
+          <polygon id="Wishlist-Icon" stroke="none" fill="#444A59" fill-rule="evenodd" points="12.3598869 13.2675869 20 13.2675869 13.8200565 17.7545318 16.1782804 25.0221187 9.99833694 20.5318477 3.81839348 25.0221187 6.17994346 17.7545318 0 13.2675869 7.63678696 13.2675869 9.99833694 6"></polygon>
+      </svg>
+    </button>
     <h3 class="md-product__name">x off-white</h3>
     <p class="md-product__description">Moletom com recorte nas mangas</p>
     <div class="md-product__pricing">
@@ -26,6 +31,7 @@
     background-color: #fff;
     padding: 20px 20px 25px;
     width: 100%;
+    position: relative;
 
     .md-product__photo {
       width: 250px;
@@ -34,6 +40,37 @@
 
       @media (min-width: 600px) {
         width: 339px;
+      }
+    }
+
+    .md-product__wish-list-button {
+      border-radius: 50%;
+      border: 2px solid $border-color;
+      width: 40px;
+      height: 40px;
+      transition: .3s ease;
+      @extend %v-align-center;
+      @extend %h-align-center;
+      position: absolute;
+      top: 10px;
+      right: 10px;
+
+      .md-wish-list__icon {
+        width: 20px;
+        height: 20px;
+        polygon {
+          transition: .3s ease;
+          fill: $border-color;
+        }
+      }
+
+      &:hover {
+        border-color: $primary-color;
+        .md-wish-list__icon {
+          polygon {
+            fill: $primary-color;
+          }
+        }
       }
     }
 
@@ -80,7 +117,7 @@
       font-size: 14px;
       font-weight: 700;
       text-align: center;
-      border: 1px solid $border-color;
+      border: 2px solid $border-color;
       border-radius: 30px;
       width: 100%;
       box-sizing: border-box;
--- src/components/modules/WishList.vue
+++ src/components/modules/WishList.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="md-wish-list">
     <svg class="md-wish-list__icon" viewBox="0 6 20 20">
-        <polygon id="Wishlist-Icon" stroke="none" fill="#444A59" fill-rule="evenodd" points="12.3598869 13.2675869 20 13.2675869 13.8200565 17.7545318 16.1782804 25.0221187 9.99833694 20.5318477 3.81839348 25.0221187 6.17994346 17.7545318 0 13.2675869 7.63678696 13.2675869 9.99833694 6"></polygon>
+        <polygon stroke="none" fill="#444A59" fill-rule="evenodd" points="12.3598869 13.2675869 20 13.2675869 13.8200565 17.7545318 16.1782804 25.0221187 9.99833694 20.5318477 3.81839348 25.0221187 6.17994346 17.7545318 0 13.2675869 7.63678696 13.2675869 9.99833694 6"></polygon>
     </svg>
     <strong class="md-mini-bag__quantity">2</strong>
   </div>
