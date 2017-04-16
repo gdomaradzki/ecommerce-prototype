3023f671f68efbde857bf2c4992c079361eeb776
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2017-04-16 07:39:43 -0300

    Fixing Node server for deploying

--- server/config/express.js
+++ server/config/express.js
@@ -1,10 +1,11 @@
 const express = require('express'),
     app = express(),
+    path = require('path'),
     bodyParser = require('body-parser'),
     consign = require('consign'),
     cors = require('cors');
 
-app.use(express.static('./../public'));
+app.use('/', express.static('./dist'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cors());
