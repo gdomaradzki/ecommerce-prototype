6328e00f2708475eaa1a0f8794e47383b82ea14e
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2017-04-16 00:20:23 -0300

    Improved unit tests for minibag and wishlist

--- test/unit/specs/App.spec.js
+++ test/unit/specs/App.spec.js
@@ -2,25 +2,41 @@ import Vue from 'vue'
 import App from '@/App'
 
 describe('App.vue', () => {
-  it('Should add price and quantity to minibag', () => {
+  it('Should add price and quantity to minibag and then subtract it', () => {
     const vm = new Vue({
       data: App.data,
       methods: App.methods
     })
 
     const product = { promo: false, price: { price: 110.95 } }
-    const productOffer = { promo: true, price: { price: 110, offer: 80 } }
 
     vm.addToCart(product)
     expect(vm.$data.miniBag.price).to.equal(product.price.price)
     expect(vm.$data.miniBag.quantity).to.equal(1)
 
-    vm.addToCart(productOffer)
-    expect(vm.$data.miniBag.price).to.equal(product.price.price + productOffer.price.offer)
-    expect(vm.$data.miniBag.quantity).to.equal(2)
+    vm.removeFromCart(product)
+    expect(vm.$data.miniBag.price).to.equal(0)
+    expect(vm.$data.miniBag.quantity).to.equal(0)
   })
 
-  it('Should add quantity to wishlist', () => {
+  it('Should add offer price and quantity to minibag and then subtract it', () => {
+    const vm = new Vue({
+      data: App.data,
+      methods: App.methods
+    })
+
+    const productOffer = { promo: true, price: { price: 110, offer: 80 } }
+
+    vm.addToCart(productOffer)
+    expect(vm.$data.miniBag.price).to.equal(productOffer.price.offer)
+    expect(vm.$data.miniBag.quantity).to.equal(1)
+
+    vm.removeFromCart(productOffer)
+    expect(vm.$data.miniBag.price).to.equal(0)
+    expect(vm.$data.miniBag.quantity).to.equal(0)
+  })
+
+  it('Should add quantity to wishlist and then remove it', () => {
     const vm = new Vue({
       data: App.data,
       methods: App.methods
@@ -29,5 +45,7 @@ describe('App.vue', () => {
     const product = { promo: false, price: { price: 110.95 } }
     vm.addToWishList(product)
     expect(vm.$data.wishList).to.equal(1)
+    vm.removeFromWishList(product)
+    expect(vm.$data.wishList).to.equal(0)
   })
 })
